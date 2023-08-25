5f0ec4b4573574a08b771059d6c552177fef4b2f
Matias <matiasanin15@gmail.com>
2023-08-25 18:54:00 -0300

    Condiciones de derrota

---
+++ Gravity Dash/Assets/Logic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Logic : MonoBehaviour
+{
+    public GameObject GameOverScreen;
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void gameOver()
+    {
+        GameOverScreen.SetActive(true);
+
+    }
+    
+}
--- Gravity Dash/Assets/Player.cs
+++ Gravity Dash/Assets/Player.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody rigid;
     public float flapforce= 50;
- 
+    public bool playerIsAlive = true;
+    public GameObject cube;
+    public Transform transform;
+
+    public Logic logic;
+
+    public string tagName = "Some tag";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +24,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) )
+        if (Input.GetKeyDown(KeyCode.Space) && playerIsAlive )
         {
             Physics.gravity = new Vector3(0, flapforce, 0);
             flapforce = (-1) * flapforce;
   
-                }
+        }
+        
+
+        transform = cube.GetComponent<Transform>();
+
+        if((transform.position.y < -6) || (transform.position.y > 12) || (transform.position.x < -36))
+        {
+            logic.gameOver();
+
+
+        }
+
+        
+        
 
     }
 }
