c9344085bb2474ebbb6988f70ec90dbaf90d3ca3
at12318 <augustotuttolomondo18@gmail.com>
2023-08-31 18:28:09 -0300

    Minor changes
    Esto seria solo el merge, todavia no está todo en su lugar.

--- Gravity Dash/Assets/scripts/menu/MainMenu.cs
+++ Gravity Dash/Assets/scripts/menu/MainMenu.cs
@@ -21,8 +21,8 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        Debug.Log(SceneManager.GetActiveScene().buildIndex + 2);
         Debug.Log("click");
     }
     /*
