e85cd06fa0965f5af9f7ea0038cd94c1946175a7
Matias <matiasanin15@gmail.com>
2023-09-07 17:09:41 -0300

    Musica

--- Gravity Dash/Assets/Logic.cs
+++ Gravity Dash/Assets/Logic.cs
@@ -8,21 +8,31 @@ public class Logic : MonoBehaviour
 {
     public GameObject GameOverScreen;
     public GameObject VictoryScreen;
+    public bool game = true;
+    public AudioSource audioSource;
+
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
+        game = true;
     }
+
+    public void music()
+    {
+        audioSource = GetComponent<AudioSource>();
+        while (game)
+        { audioSource.Play(); }
+        }
 
     public void gameOver()
     {
         GameOverScreen.SetActive(true);
-
+        game = false;
     }
 
     public void victory()
     {
         VictoryScreen.SetActive(true);
-
+        game = false;
     }
 }
